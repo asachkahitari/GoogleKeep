22d0a6ca2c39c5c487f643ea4816b214f9ba24b6
Deepak Patil <patildeep2000@gmail.com>
2021-06-05 12:45:46 +0530

    delete icon styling changed

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import DeleteIcon from '@material-ui/icons/Delete';
+import Fab from '@material-ui/core/Fab';
 function Note(props) {
   function handleClick(){
     props.onDelete(props.id);
@@ -8,7 +9,7 @@ function Note(props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}><DeleteIcon /></button>
+      <Fab onClick={handleClick}><DeleteIcon /></Fab>
     </div>
   );
 }
